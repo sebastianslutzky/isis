efceffe726580569ceb875b40d0fb8938ad56c07
Dan Haywood <dan@haywood-associates.co.uk>
2014-12-30 14:56:16 +0000

    ISIS-970: remove support for @ActionLayout(prototype=...) and parsing from xxx.layout.json; add support for @Action(restrictTo=Environment[]).

--- example/application/todoapp/dom/src/main/java/dom/todo/ToDoItem.java
+++ example/application/todoapp/dom/src/main/java/dom/todo/ToDoItem.java
@@ -48,6 +48,7 @@ import org.apache.isis.applib.annotation.CollectionLayout;
 import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.DomainObjectLayout;
 import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.Environment;
 import org.apache.isis.applib.annotation.InvokeOn;
 import org.apache.isis.applib.annotation.InvokedOn;
 import org.apache.isis.applib.annotation.Optionality;
@@ -687,8 +688,10 @@ public class ToDoItem implements Categorized, Comparable<ToDoItem> {
     //endregion
 
     //region > openSourceCodeOnGithub (action)
-    @Action(semantics = SemanticsOf.SAFE)
-    @ActionLayout(prototype = true)
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = Environment.DEVELOPMENT
+    )
     public URL openSourceCodeOnGithub() throws MalformedURLException {
         return new URL("https://github.com/apache/isis/tree/master/example/application/todoapp/dom/src/main/java/dom/todo/ToDoItem.java");
     }
@@ -702,8 +705,10 @@ public class ToDoItem implements Categorized, Comparable<ToDoItem> {
         NonRecoverableException;
     }
 
-    @Action(semantics = SemanticsOf.SAFE)
-    @ActionLayout(prototype = true)
+    @Action(
+            semantics = SemanticsOf.SAFE,
+            restrictTo = Environment.DEVELOPMENT
+    )
     public void demoException(
             @ParameterLayout(named="Type")
             final DemoExceptionType type) {
